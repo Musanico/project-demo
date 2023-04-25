d30fd05e0947ae487ae350251a8cd58753e4d643
Musanico <108330876+Musanico@users.noreply.github.com>
2023-04-25 14:40:18 +0800

    4

--- src/pages/PersonPage1/index.js
+++ src/pages/PersonPage1/index.js
@@ -2,6 +2,7 @@ import "@/pages/PersonPage1/index.css"
 import Nav from '@/components/nav'
 import PersonSidebar from '@/components/person-sidebar'
 import ScrollLock from 'react-scrolllock';
+import {MailOutlined} from '@ant-design/icons'
 
 function Tperson1(){
   return(
@@ -16,6 +17,9 @@ function Tperson1(){
             <div>
               <img src={require("@/assets/example.png")}  className="tp1-avatar" alt=""/>
             </div>
+            <div className="tp1-name">匡振中</div>
+            <div className="tp1-email"><MailOutlined />  zzkuang@hdu.edu.cn</div>
+            <div className="tp1-intro">匡振中，男，1987年生，山东日照人，副研究员，硕士生导师。于2017年6月获得中国石油大学与美国北卡罗莱纳大学联合培养博士学位，自2017年10月起任职于杭州电子科技大学计算机学院，是复杂系统建模与仿真教育部重点实验室成员，属于浙江省高水平创新团队成员，入选杭州电子科技大学优秀骨干教师支持计划。</div>
           </div>
       </div>
   )
--- src/pages/PersonPage2/index.js
+++ src/pages/PersonPage2/index.js
@@ -1,13 +1,21 @@
 import "@/pages/PersonPage2/index.css"
 import Nav from '@/components/nav'
 import PersonSidebar from '@/components/person-sidebar'
+import ScrollLock from 'react-scrolllock'
 
 function Tperson2(){
   return(
       <div>
         <Nav/>
-        <div className="tp2-sidebar">
-          <PersonSidebar/>
+        <ScrollLock>
+          <div className="tp2-sidebar">
+            <PersonSidebar/>
+          </div>
+        </ScrollLock>
+        <div className="tp2-main">
+          <div className="tp2-intro">
+            从事人工智能分支中视觉识别技术研究，主要采用计算机视觉和机器学习方法（如深度学习技术）去认识和理解包括二维图像和三维模型在内的可视化媒体，涉及层次大规模图像识别、图像隐私保护以及三维模型检索等。在国内外知名期刊和国际会议上共发表论文20余篇（例如PAMI、TIFS、TIP、PR、PRL、CAD等）。承担中央高校自主创新课题1项，参与包括国家自然基金以及山东基金在内的多项课题。参加的高水平国际会议，包括ACM Siggraph Asia、SPM、ICMR以及SMI等，获得CAD/Graphics 2015国际会议最佳论文奖。担任多个国际期刊审稿人，如TNNLS、Information Sciences、ICMR、MTA以及MultimediaSystem等。
+          </div>
         </div>
       </div>
   )
